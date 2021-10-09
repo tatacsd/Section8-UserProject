57d93cb4015c19b710c251bdc935aea6770bbe53
thays <thayscasado@hotmail.com>
2021-10-09 00:39:13 -0700

    94. Adding a Users List Component

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../UI/Cards";
+import css from "./UsersList.module.css";
+const UserList = (props) => {
+  return (
+    <Card className={css.users}>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li>
+              {user.name} ({user.age} years old)
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
